c92fe3277a9920b62c6e3a2508ef8a735ee136c2
Gwen <gwen.munson15@gmail.com>
2025-11-12 20:26:05 -0500

    committing these because i changed repo name

--- VeiledSpeechPrototype/VeiledSpeechPrototype/obj/Debug/net8.0/VeiledSpeechPrototype.AssemblyInfo.cs
+++ VeiledSpeechPrototype/VeiledSpeechPrototype/obj/Debug/net8.0/VeiledSpeechPrototype.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VeiledSpeechPrototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2ebb497f348a64f71e8488c8e28acc236464256")]
 [assembly: System.Reflection.AssemblyProductAttribute("VeiledSpeechPrototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VeiledSpeechPrototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
